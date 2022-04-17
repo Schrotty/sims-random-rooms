9052af03fd7893bad8cfd4469a2a8c3f50678853
Ruben Maurer <rubenmaurer@live.de>
2022-04-17 21:18:55 +0200

    Update room generation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,23 @@
-function f() {
-    const min = parseInt((<HTMLInputElement>document.getElementById("min-x")).value)
-    const max = parseFloat((<HTMLInputElement>document.getElementById("max-x")).value)
+function random_room() {
+    const measurements = <HTMLSpanElement>document.getElementById("measurements")
+    const cheatcode = <HTMLSpanElement>document.getElementById("cheatcode")
+    const money = <HTMLSpanElement>document.getElementById("money")
 
-    const x_value = Math.floor(Math.random() * (max - min + 1)) + min
-    const y_value = Math.floor(Math.random() * (max - min + 1)) + min
+    // room size values
+    const s_min = parseInt((<HTMLInputElement>document.getElementById("min-x")).value)
+    const s_max = parseFloat((<HTMLInputElement>document.getElementById("max-x")).value)
 
-    alert(String("Your room has the size: " + x_value + "x" + y_value));
+    // money values
+    const m_min = parseInt((<HTMLInputElement>document.getElementById("money-min")).value)
+    const m_max = parseInt((<HTMLInputElement>document.getElementById("money-max")).value)
+
+    // calc room size & money
+    const x_value = Math.floor(Math.random() * (s_max - s_min + 1)) + s_min
+    const y_value = Math.floor(Math.random() * (s_max - s_min + 1)) + s_min
+    const m = Math.floor(Math.random() * (m_max - m_min + 1)) + m_min
+
+    measurements.textContent = String(x_value + "x" + y_value + "m")
+    money.textContent = String(m + "§")
+    cheatcode.textContent = String("Money " + m)
 }
 
