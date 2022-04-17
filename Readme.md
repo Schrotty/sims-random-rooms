2c328f8f8d521c1e12fd0c489ef9b7fd71b9ba01
Ruben Maurer <rubenmaurer@live.de>
2022-04-17 19:07:28 +0200

    Adding demo

--- Readme.md
+++
@@ -1,5 +0,0 @@
-# Sims Random Rooms
-A very basic tool for the "Every Room is Random" challenge in Sims.
-
-## License
-MIT
---
+++ index.a4a17d7e.js
@@ -0,0 +1,2 @@
+function f(){const t=parseInt(document.getElementById("min-x").value),e=parseFloat(document.getElementById("max-x").value),a=Math.floor(Math.random()*(e-t+1))+t,o=Math.floor(Math.random()*(e-t+1))+t;alert(String("Your room has the size: "+a+"x"+o))}
+//# sourceMappingURL=index.a4a17d7e.js.map
